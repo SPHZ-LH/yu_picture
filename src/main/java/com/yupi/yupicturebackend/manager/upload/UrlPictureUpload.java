6931402fb1d9a259bd1be7d7190091ed5cd078dd
sphz <sphz@qq.com>
2026-01-27 22:56:36 +0800

    fix：修复URL上传图片没有后缀名，URL上传图片大小限制为10MB

--- src/main/java/com/yupi/yupicturebackend/manager/upload/UrlPictureUpload.java
+++ src/main/java/com/yupi/yupicturebackend/manager/upload/UrlPictureUpload.java
@@ -63,8 +63,8 @@ public class UrlPictureUpload extends PictureUploadTemplate {
             if (StrUtil.isNotBlank(contentLengthStr)) {
                 try {
                     long contentLength = Long.parseLong(contentLengthStr);
-                    final long TWO_MB = 2 * 1024 * 1024L; // 限制文件大小为 2MB
-                    ThrowUtils.throwIf(contentLength > TWO_MB, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2M");
+                    final long TWO_MB = 10 * 1024 * 1024L; // 限制文件大小为 10MB
+                    ThrowUtils.throwIf(contentLength > TWO_MB, ErrorCode.PARAMS_ERROR, "文件大小不能超过 10MB");
                 } catch (NumberFormatException e) {
                     throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件大小格式错误");
                 }
@@ -82,7 +82,7 @@ public class UrlPictureUpload extends PictureUploadTemplate {
     @Override
     protected String getOriginFilename(Object inputSource) {
         String fileUrl = (String) inputSource;
-        return FileUtil.mainName(fileUrl);
+        return FileUtil.getName(fileUrl);
     }
 
     /**
